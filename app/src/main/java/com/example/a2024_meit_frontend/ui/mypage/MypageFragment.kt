03820f79f698c9fa7e21b5913b2e72bee4d87923
minzero31 <77minyoung77@gmail.com>
2024-09-29 02:15:21 +0900

    rename Tab name & Package Name #1

--- app/src/main/java/com/example/a2024_meit_frontend/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/a2024_meit_frontend/ui/mypage/MypageFragment.kt
@@ -1,4 +1,4 @@
-package com.example.a2024_meit_frontend.ui.management
+package com.example.a2024_meit_frontend.ui.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.a2024_meit_frontend.databinding.FragmentDashboardBinding
+import com.example.a2024_meit_frontend.databinding.FragmentNotificationsBinding
 
-class DashboardFragment : Fragment() {
+class MypageFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentNotificationsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(MypageViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
