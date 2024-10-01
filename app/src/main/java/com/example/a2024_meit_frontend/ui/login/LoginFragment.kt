79b705cd2778cf0788fab5a90aa52e466f9ca127
minzero31 <77minyoung77@gmail.com>
2024-10-02 07:15:09 +0900

    Feature : 온보딩 fragment에서 로그인 fragment 전환 완료 & 로그인 layout 디자인 완료 #6 #2

--- app/src/main/java/com/example/a2024_meit_frontend/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/a2024_meit_frontend/ui/login/LoginFragment.kt
@@ -1,4 +1,20 @@
 package com.example.a2024_meit_frontend.ui.login
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a2024_meit_frontend.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // fragment_login.xml 레이아웃을 인플레이트합니다.
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
 }
